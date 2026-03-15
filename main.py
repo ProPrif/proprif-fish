7a0509d132237ed1521ec8d255ba070f60e6f40d
manta <mantas.zaliaduonis@gmail.com>
2026-03-15 18:32:46 +0200

    atliktas subtask KAN-31

--- main.py
+++ main.py
@@ -14,7 +14,6 @@ def get_selected_fish():
     ids = input("\nĮveskite žuvų numerius (pvz: 1,2): ")
 
     ids = ids.split(",")
-
     selected = []
 
     for id in ids:
@@ -32,35 +31,40 @@ def check_compatibility(selected_fish):
         print("Reikia pasirinkti bent 2 žuvis.")
         return None
 
-    # paprasta simulacija
     result = "Suderinamos"
-
     return result
 
 
 def save_to_history(selected_fish, result):
     time = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
-
     fish_names = [fish["name"] for fish in selected_fish]
 
-    line = f"{time} | Žuvys: {', '.join(fish_names)} | Rezultatas: {result}\n"
+    new_line = f"{time} | Žuvys: {', '.join(fish_names)} | Rezultatas: {result}\n"
 
-    with open("history.txt", "a", encoding="utf-8") as file:
-        file.write(line)
+    try:
+        with open("history.txt", "r", encoding="utf-8") as file:
+            lines = file.readlines()
+    except FileNotFoundError:
+        lines = []
+
+    lines.append(new_line)
+
+    if len(lines) > 10:
+        lines = lines[-10:]
+
+    with open("history.txt", "w", encoding="utf-8") as file:
+        file.writelines(lines)
 
 
 def main():
     show_fish_list()
 
     selected_fish = get_selected_fish()
-
     result = check_compatibility(selected_fish)
 
     if result:
         print("\nRezultatas:", result)
-
         save_to_history(selected_fish, result)
-
         print("Rezultatas išsaugotas į history.txt")
 
 
